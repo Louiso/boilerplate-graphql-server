15cd17c56a45d052d9d51e4b8aed8bb3e9b2f964
LuisSullca <luis.sullca.h@uni.pe>
2021-08-09 19:05:38 -0500

    [update] se corrigio validacion de conneccion

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:12
+FROM node:14
 
 ARG NODE_ENV=production
 ENV NODE_ENV $NODE_ENV
--- src/config/connections.ts
+++ src/config/connections.ts
@@ -1,9 +1,9 @@
 import mongoose from 'mongoose'
 
-export const connection = mongoose.createConnection(
+export const connection = process.env.MONGO_CONNECTION ? mongoose.createConnection(
   process.env.MONGO_CONNECTION as string, {
     useUnifiedTopology: true,
     socketTimeoutMS   : 0,
     keepAlive         : true
   }
-)
+) : null
