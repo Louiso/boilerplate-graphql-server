124394ae1c7bcee8f48b3fc3ec6d3f0085757577
Louiso <gransullca.25@gmail.com>
2022-02-12 12:58:56 -0500

    []

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,10 @@
 import signale from 'signale'
 
-import { ApolloServer } from 'apollo-server'
-// import express from 'apollo-server-express/node_modules/@types/express'
+import { ApolloServer } from 'apollo-server-express'
 
 import schema from './service/graphql/schema'
-// import { datasources } from './graphql/extentions/datasources'
 
-// import app from './service/api/app'
+import app from './service/api/app'
 
 import './config/connections'
 import context from './service/graphql/context'
@@ -21,27 +19,19 @@ const server = new ApolloServer({
   introspection: true
 })
 
-server.listen({
-  port: PORT
-})
-  .then(({ url }) => {
-    signale.success(`[Krowdy] Server ready at ${url}`)
+const main = async function() {
+  await server.start()
+
+  server.applyMiddleware({
+    app
+  })
+
+  app.listen(PORT, () => {
+    signale.success(`[Krowdy] Server ready at http://localhost:${PORT}/graphql`)
     signale.info(`[KROWDY] SERVER RUN WITH ${process.env.NODE_OPTIONS}`)
     signale.info(`Try your health check at: http://localhost:${PORT}/graphql.well-known/apollo/server-health`)
   })
-// const main = async function() {
-//   await server.start()
+}
 
-//   server.applyMiddleware({
-//     app: app as express.Application
-//   })
-
-//   app.listen(PORT, () => {
-//     signale.success(`[Krowdy] Server ready at http://localhost:${PORT}/graphql`)
-//     signale.info(`[KROWDY] SERVER RUN WITH ${process.env.NODE_OPTIONS}`)
-//     signale.info(`Try your health check at: http://localhost:${PORT}/graphql.well-known/apollo/server-health`)
-//   })
-// }
-
-// main()
+main()
 
